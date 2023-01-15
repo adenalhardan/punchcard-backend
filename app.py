34767e11989de8419c2705c615682a6ba742e386
Aden  Al-Hardan <adenalhardan@gmail.com>
2023-01-14 16:57:13 -0800

    initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from mangum import Mangum
+
+import random
+import string
+
+app = FastAPI()
+handler = Mangum(app)
+
+prefix = 'punchcard:'
+
+@app.get('/')
+async def root():
+    return {'message': 'all good'}
+
+@app.get('/get-name')
+async def get_name():
+    name = ''.join(random.choices(string.ascii_letters + string.digits, k = 5))
+    return {'name': prefix + name}
