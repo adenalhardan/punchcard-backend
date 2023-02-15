09dfc15b9211644e7ab856886c5c8fb84b76bb71
Aden  Al-Hardan <adenalhardan@gmail.com>
2023-02-14 21:38:20 -0500

    debug

--- app.py
+++ app.py
@@ -179,7 +179,7 @@ async def get_events(host_id: str):
 
 @app.delete('/delete-event')
 async def delete_event(host_id: str, event_title: str):
-    event_title = urllib.unquote_plus(event_title)
+    event_title = urllib.parse.unquote_plus(event_title)
 
     response = execute(f'SELECT * FROM punchcard.event WHERE host_id = "{host_id}" AND title = "{event_title}"', 'GET')
 
