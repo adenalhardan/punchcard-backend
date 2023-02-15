480ce77c56dad393472de031f473189063ebe404
Aden  Al-Hardan <adenalhardan@gmail.com>
2023-02-14 22:00:00 -0500

    debug

--- app.py
+++ app.py
@@ -36,8 +36,6 @@ def execute(sql, type, args = []):
         parameters = args
     )
 
-    return response
-
     if type in ['POST', 'UPDATE', 'DELETE']:
         if response['ResponseMetadata']['HTTPStatusCode'] == 200:
             return {'status': 'success'}  
@@ -183,11 +181,16 @@ async def get_events(host_id: str):
 async def delete_event(host_id: str, event_title: str):
     event_title = urllib.parse.unquote_plus(event_title)
 
-    response = execute(f'SELECT * FROM punchcard.event WHERE host_id = "{host_id}" AND title = "{event_title}"', 'GET')
-    return response
+    #response = execute(f'SELECT * FROM punchcard.event WHERE host_id = "{host_id}" AND title = "{event_title}"', 'GET')
+    
     #if len(response) == 0:
         #return {'status': 'error', 'message': 'event does not exist'}
-
+    return rds_client.execute_statement(
+            secretArn = params['database_credentials_secret_store_arn'],
+            database = params['database_name'],
+            resourceArn = params['database_cluster_arn'],
+            sql = f'DELETE FROM punchcard.event WHERE host_id = "{host_id}" AND title = "{event_title}"',
+        )
     event_response = execute(f'DELETE FROM punchcard.event WHERE host_id = "{host_id}" AND title = "{event_title}"', 'DELETE')
     form_response = execute(f'DELETE FROM punchcard.form WHERE host_id = "{host_id}" AND event_title = "{event_title}"', 'DELETE')
 
